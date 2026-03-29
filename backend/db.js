3af3530d54717f852abae8233e6d322b980c6bf1
KASHADII <kashyap.aditya83760@gmail.com>
2026-03-29 14:43:37 +0530

    Changes

--- backend/db.js
+++ backend/db.js
@@ -6,6 +6,10 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  lookup: (hostname, options, callback) => {
+    const opts = typeof options === 'number' ? { family: options } : (options || {});
+    return dns.lookup(hostname, { ...opts, family: 4 }, callback);
+  },
   ssl: {
     rejectUnauthorized: false
   }
